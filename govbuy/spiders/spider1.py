c20021639358202af65880f94e0f864e2f187d8d
ahlinjie <1051631394@qq.com>
2016-11-26 18:00:32 +0800

    add a simple spider

---
+++ govbuy/spiders/spider1.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import
+import logging
+import scrapy
+
+logger = logging.getLogger(__name__)
+
+
+class GovBuySpider(scrapy.Spider):
+    name = 'govbuy'
+    host = 'http://www.shucheng.gov.cn'
+
+    def start_requests(self):
+        urls = [
+            '{0}/tmp/xxgklist2.shtml?unitsId=1&action=list&types=2&SS_Path=/-012-148-052&SSIDS=2012',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url.format(self.host), callback=self.prase)
+
+    def prase(self, response):
+        filename = 'test.html'
+        links = response.xpath('//li[re:test(@class, "mc$")]//a')
+        for index, link in enumerate(links):
+            shref = link.xpath('@href').extract()
+            sname = link.xpath('text()').extract()
+            href = shref[0] if shref else None
+            name = sname[0].strip() if sname else None
+            print u'name: %s \n url: %s ' % (name, href)
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('save file %s' % filename)
